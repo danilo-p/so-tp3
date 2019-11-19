a3fd129d581beffdd028f9ef9a2090bfc980cbce
Danilo Pimentel <danilo.pimentel97@gmail.com>
2019-11-18 22:23:15 -0300

    Otimiza compilação e implementa ticks de clock.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: build
 
 build:
-	gcc -Wall -o tp3virtual main.c
+	gcc -Wall -O3 -o tp3virtual main.c
 	chmod a+x tp3virtual
 
 clean:
--- main.c
+++ main.c
@@ -7,13 +7,15 @@
 
 typedef struct
 {
-    int valid;
-    int dirty;
-    time_t timestamp;
-    time_t last_used_ms;
+    int valid_bit;
+    int dirty_bit;
+    unsigned placed_in_memory_at;
+    unsigned last_used_at;
     int reference_bit;
-    int received_chance;
+    int received_chance_bit;
 } page_t;
+
+unsigned ticks;
 
 unsigned get_addr_shift(unsigned page_size_kb)
 {
@@ -34,9 +36,10 @@ unsigned get_target_page(unsigned addr, unsigned addr_shift)
 
 int strategy_dummy(page_t *page_table)
 {
-    for (int i = 0; i < PAGE_TABLE_SIZE; i++)
+    int i;
+    for (i = 0; i < PAGE_TABLE_SIZE; i++)
     {
-        if (page_table[i].valid)
+        if (page_table[i].valid_bit)
         {
             return i;
         }
@@ -46,70 +49,70 @@ int strategy_dummy(page_t *page_table)
 
 int strategy_lru(page_t *page_table)
 {
-    int min_i = -1;
-    for (int i = 0; i < PAGE_TABLE_SIZE; i++)
+    int victim_page = -1, i;
+    for (i = 0; i < PAGE_TABLE_SIZE; i++)
     {
-        if (page_table[i].valid)
+        if (page_table[i].valid_bit)
         {
-            if (min_i == -1 || difftime(page_table[min_i].last_used_ms, page_table[i].last_used_ms) > 0)
+            if (victim_page == -1 || page_table[victim_page].last_used_at > page_table[i].last_used_at)
             {
-                min_i = i;
+                victim_page = i;
             }
         }
     }
-    return min_i;
+    return victim_page;
 }
 
 int strategy_2a(page_t *page_table)
 {
-    int victm_page = 0, i = 0, selected_page = 0;
-    time_t time_now = time(NULL);
+    int victim_page = 0, i, selected_page = 0;
+    int time_now = ticks + 1;
     double diff = 0.0;
 
     for (i = 0; i < PAGE_TABLE_SIZE; i++)
     {
-        page_table[i].received_chance = 0;
+        page_table[i].received_chance_bit = 0;
     }
 
     while (1)
     { //gives second chance
         for (i = 0; i < PAGE_TABLE_SIZE; i++)
         { //finds page
-            if (page_table[i].valid && !page_table[i].received_chance && difftime(time_now, page_table[i].timestamp) > diff)
+            if (page_table[i].valid_bit && !page_table[i].received_chance_bit && time_now - page_table[i].placed_in_memory_at > diff)
             {
                 selected_page = i;
-                diff = difftime(time_now, page_table[i].timestamp);
+                diff = time_now - page_table[i].placed_in_memory_at;
             }
         }
 
         if (page_table[selected_page].reference_bit == 0)
         {
-            victm_page = selected_page;
+            victim_page = selected_page;
             break;
         }
         else
         {
             page_table[selected_page].reference_bit = 0;
-            page_table[selected_page].received_chance = 1;
+            page_table[selected_page].received_chance_bit = 1;
         }
     }
-    return victm_page;
+    return victim_page;
 }
 
 int strategy_fifo(page_t *page_table)
 {
-    int victm_page = 0, i = 0;
-    time_t time_now = time(NULL);
+    int victim_page = 0, i;
+    int time_now = ticks + 1;
     double diff = 0.0;
     for (i = 0; i < PAGE_TABLE_SIZE; i++)
     {
-        if (page_table[i].valid && difftime(time_now, page_table[i].timestamp) > diff)
+        if (page_table[i].valid_bit && time_now - page_table[i].placed_in_memory_at > diff)
         {
-            victm_page = i;
-            diff = difftime(time_now, page_table[i].timestamp);
+            victim_page = i;
+            diff = time_now - page_table[i].placed_in_memory_at;
         }
     }
-    return victm_page;
+    return victim_page;
 }
 
 int strategy_random(page_t *page_table)
@@ -118,7 +121,7 @@ int strategy_random(page_t *page_table)
     do
     {
         victim_page = (random() % PAGE_TABLE_SIZE);
-    } while (!page_table[victim_page].valid);
+    } while (!page_table[victim_page].valid_bit);
 
     return victim_page;
 }
@@ -170,14 +173,17 @@ int main(int argc, char *argv[])
     printf("Tecnica de reposicao: %s\n", swap_strategy);
 
     page_t *page_table = malloc(PAGE_TABLE_SIZE * sizeof(page_t));
-    for (int i = 0; i < PAGE_TABLE_SIZE; i++)
+    int i;
+    for (i = 0; i < PAGE_TABLE_SIZE; i++)
     {
-        page_table[i].valid = 0;
-        page_table[i].dirty = 0;
+        page_table[i].valid_bit = 0;
+        page_table[i].dirty_bit = 0;
         page_table[i].reference_bit = 1;
     }
 
     FILE *addr_file = fopen(addr_file_name, "r");
+
+    ticks = 0;
 
     int page_faults = 0;
     int dirty_writes = 0;
@@ -190,19 +196,20 @@ int main(int argc, char *argv[])
     unsigned addr_shift = get_addr_shift(page_size_kb);
     unsigned target_page;
     unsigned victim_page;
-    while (!feof(addr_file))
+    while (fscanf(addr_file, "%x %c", &addr, &rw) != EOF)
     {
-        fscanf(addr_file, "%x %c", &addr, &rw);
+        ticks++;
+
         target_page = get_target_page(addr, addr_shift);
 
         if (rw == 'W')
         {
-            page_table[target_page].dirty = 1;
+            page_table[target_page].dirty_bit = 1;
         }
 
-        page_table[target_page].last_used_ms = time(NULL);
+        page_table[target_page].last_used_at = ticks;
 
-        if (page_table[target_page].valid)
+        if (page_table[target_page].valid_bit)
         {
             continue;
         }
@@ -211,23 +218,23 @@ int main(int argc, char *argv[])
 
         if (frames_filled < frames_total)
         {
-            page_table[target_page].valid = 1;
-            page_table[target_page].timestamp = time(NULL);
+            page_table[target_page].valid_bit = 1;
+            page_table[target_page].placed_in_memory_at = ticks;
             page_table[target_page].reference_bit = 1;
             frames_filled++;
             continue;
         }
 
         victim_page = get_victim_page(page_table, swap_strategy);
-        if (page_table[victim_page].dirty)
+        if (page_table[victim_page].dirty_bit)
         {
             dirty_writes++;
         }
-        page_table[victim_page].valid = 0;
-        page_table[victim_page].dirty = 0;
+        page_table[victim_page].valid_bit = 0;
+        page_table[victim_page].dirty_bit = 0;
 
-        page_table[target_page].valid = 1;
-        page_table[target_page].timestamp = time(NULL);
+        page_table[target_page].valid_bit = 1;
+        page_table[target_page].placed_in_memory_at = ticks;
         page_table[target_page].reference_bit = 1;
     }
 
